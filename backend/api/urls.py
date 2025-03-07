4362d122122ff381b3c43b727d8c43da00f0bdf4
Sebastian Corcino <corcinosebastian@outlook.com>
2025-03-07 11:16:49 -0900

    update data structure with foreign keys

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import EnrolledCoursesView, CourseAnswersView
+from .views import StudentEnrolledCourseView, StudentSurveyQuestionsView, StudentSubmitSurveyAnswerView
+from .views import FacultyViewAnswersView
 
 urlpatterns = [
-    path('courses', EnrolledCoursesView.as_view(), name='get_enrolled_courses'),
-    path('course-answers', CourseAnswersView.as_view(), name="get_courses_answers")
+    # Student endpoints
+    path('student/enrolled-courses', StudentEnrolledCourseView.as_view(), name='get-student-enrolled-courses'),
+    path('student/survey-questions/<int:crn>/<int:term>/', StudentSurveyQuestionsView.as_view(), name='get_enrolled_courses'),
+    path('student/submit-course-answers', StudentSubmitSurveyAnswerView.as_view(), name="post-student-course-answers"),
+
+    # Faculty endpoints
+    path('faculty/view-answers', FacultyViewAnswersView.as_view(), name='get-faculty-view-answers'),
+
+    # Admin endpoints
 ]
