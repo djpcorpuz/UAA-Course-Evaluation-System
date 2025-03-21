3451fe5fd1ad43e26788bcc4cea0f4ab46af5094
djpcorpuz <dpcorpuz@alaska.edu>
2025-03-20 23:30:28 -0800

    Renamed variables for clarificatio

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
 # MyModel = Custom Permissions Table? Need to confirm
-class MyModel(models.Model):
+class CustomPermissions(models.Model):
     name = models.CharField(max_length = 100)                   # Name of custom permission
     description = models.TextField()                            # Description of custom permission
 
@@ -12,13 +12,14 @@ class MyModel(models.Model):
     class Meta:
         permissions = [
             #System Admin
-            ('set_system_admin','promote to system admin')
+            ('set_system_admin','set to system admin')          # sets user to system admin
+            ('set_admin','set to admin'),                       # sets user to admin
             #Admin
             ('approve','can approve set'),                      # approve custom question set
             ('reject','can reject set'),                        # reject custom question set
             ('set_timelock','can set timelocks'),               # sets timelocks for editing/modifying question sets, submitting answers, etc
-            ('set_faculty','promote to faculty'),
-            ('set_student','promote to student'),
+            ('set_faculty','set to faculty'),                   # sets user to faculty
+            ('set_student','set to student'),                   # sets user to student (may remove since this should be default)
             #Faculty
             ('view_records','can view submitted answers'),
             ('edit_set','can edit set'),
@@ -29,14 +30,14 @@ class MyModel(models.Model):
 class RoleSetup:
     @staticmethod
     def create_roles_and_permissions():
-        content_type = ContentType.objects.get_for_model(MyModel)
+        content_type = ContentType.objects.get_for_model(CustomPermissions)
 
         #Initializing permissions
         view_perm = Permission.objects.get(codename='view_mymodel', content_type=content_type)
         add_perm = Permission.objects.get(codename='add_mymodel', content_type=content_type)
         change_perm = Permission.objects.get(codename='change_mymodel', content_type=content_type)
         delete_perm = Permission.objects.get(codename='delete_mymodel', content_type=content_type)
-        #add remaining perms
+        # TODO: add remaining perms
 
         #Initializing roles
         system_admin_group, created = Group.objects.get_or_create(name='System Admin')
@@ -48,6 +49,7 @@ class RoleSetup:
         permissions = Permission.objects.filter(content_type__app_label='auth', content_type__model='user')
 
          ## TO DO ## 
+
             # System Admin 
         system_admin_group.permissions.set(permissions)
 
